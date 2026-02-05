db1014f0e372303348a6f66e5f48a21b98574091
hermaplusplus <49307226+hermaplusplus@users.noreply.github.com>
2026-02-05 22:58:37 +0000

    Add util module

---
+++ utils.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import subprocess
+import platform
+import os
+
+def add_clean_footer():
+    try:
+        branch = subprocess.check_output(["git", "rev-parse", "--abbrev-ref", "HEAD"]).decode().strip()
+    except:
+        branch = "Main"
+    
+    is_docker = "Active" if os.path.exists('/.dockerenv') else "Inactive"
+    
+    st.markdown("---")
+    st.markdown(f"Made with ❤️ by [Herma](https://github.com/hermaplusplus/Arwen) | Provided under the [MIT License](https://github.com/hermaplusplus/Arwen/blob/main/LICENSE)", text_alignment="center")
+    st.markdown(f"Branch: {branch} | Docker: {is_docker} | Python: {platform.python_version()}", text_alignment="center")
