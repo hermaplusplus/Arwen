24078ccc50eec2b85840c491df780e9ee685c6c3
hermaplusplus <49307226+hermaplusplus@users.noreply.github.com>
2026-02-06 02:08:42 +0000

    Fix git branch and commit display

--- utils.py
+++ utils.py
@@ -4,17 +4,29 @@ import platform
 import os
 
 def add_clean_footer():
-    try:
-        branch = subprocess.check_output(["git", "rev-parse", "--abbrev-ref", "HEAD"]).decode().strip()
-        branch = f":green-badge[**{branch}**]" if branch == "main" else f":orange-badge[**{branch}**]"
-    except:
-        branch = ":red-badge[UNKNOWN]"
+
+    branch = os.environ.get("GIT_BRANCH", "UNKNOWN")
+    branch_url = f"https://github.com/hermaplusplus/Arwen/tree/{branch}"
+    commit = os.environ.get("GIT_COMMIT", "UNKNOWN")
+    commit_url = f"https://github.com/hermaplusplus/Arwen/commit/{commit}"
+
+    if branch != "UNKNOWN":
+        branch = f":green-badge[**[{branch}]({branch_url})**]" if branch == "main" else f":orange-badge[**[{branch}]({branch_url})**]"
+    else:
+        try:
+            branch = subprocess.check_output(["git", "rev-parse", "--abbrev-ref", "HEAD"]).decode().strip()
+            branch = f":green-badge[**[{branch}]({branch_url})**]" if branch == "main" else f":orange-badge[**[{branch}]({branch_url})**]"
+        except:
+            branch = ":red-badge[UNKNOWN]"
     
-    try:
-        commit = subprocess.check_output(["git", "rev-parse", "--short", "HEAD"]).decode().strip()
-        commit = f":blue-badge[**{commit}**]"
-    except:
-        commit = ":red-badge[UNKNOWN]"
+    if commit != "UNKNOWN":
+        commit = f":blue-badge[**[{commit}]({commit_url})**]"
+    else:
+        try:
+            commit = subprocess.check_output(["git", "rev-parse", "--short", "HEAD"]).decode().strip()
+            commit = f":blue-badge[**[{commit}]({commit_url})**]"
+        except:
+            commit = ":red-badge[UNKNOWN]"
     
     is_docker = ":green-badge[ACTIVE]" if os.path.exists('/.dockerenv') else ":red-badge[INACTIVE]"
     
