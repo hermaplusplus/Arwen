aa93e7032eac23404d2ec21dea2e81403fa4fcbc
hermaplusplus <49307226+hermaplusplus@users.noreply.github.com>
2026-02-06 01:35:29 +0000

    Add commit info and styles to footer

--- utils.py
+++ utils.py
@@ -6,11 +6,18 @@ import os
 def add_clean_footer():
     try:
         branch = subprocess.check_output(["git", "rev-parse", "--abbrev-ref", "HEAD"]).decode().strip()
+        branch = f":green-badge[**{branch}**]" if branch == "main" else f":orange-badge[**{branch}**]"
     except:
         branch = ":red-badge[UNKNOWN]"
     
-    is_docker = "Active" if os.path.exists('/.dockerenv') else "Inactive"
+    try:
+        commit = subprocess.check_output(["git", "rev-parse", "--short", "HEAD"]).decode().strip()
+        commit = f":blue-badge[**{commit}**]"
+    except:
+        commit = ":red-badge[UNKNOWN]"
+    
+    is_docker = ":green-badge[ACTIVE]" if os.path.exists('/.dockerenv') else ":red-badge[INACTIVE]"
     
     st.markdown("---")
     st.markdown(f"Made with ❤️ by [Herma](https://github.com/hermaplusplus/Arwen) | Provided under the [MIT License](https://github.com/hermaplusplus/Arwen/blob/main/LICENSE)", text_alignment="center")
-    st.markdown(f"Branch: {branch} | Docker: {is_docker} | Python: {platform.python_version()}", text_alignment="center")
+    st.markdown(f"Branch: {branch}@{commit} | Docker: {is_docker} | Python: :blue-badge[{platform.python_version()}]", text_alignment="center")
