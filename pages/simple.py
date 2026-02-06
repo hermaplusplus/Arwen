f2db38431f2036573caa5cf4a3fc8dc26d354a4c
hermaplusplus <49307226+hermaplusplus@users.noreply.github.com>
2026-02-06 01:22:30 +0000

    Add return buttons and footer to simple template page

--- pages/simple.py
+++ pages/simple.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import yaml
+from utils import add_clean_footer
 
 # Safety check
 if "current_dis" not in st.session_state:
@@ -7,8 +8,14 @@ if "current_dis" not in st.session_state:
 
 st.title(f"{st.session_state.current_dis}")
 
+# Back button
+if st.button("<- Return to Homepage", key="backtop"):
+    st.switch_page("main.py")
+
 
 
 # Back button
-if st.button("<- Change Disorder"):
+if st.button("<- Return to Homepage", key="backbottom"):
     st.switch_page("main.py")
+
+add_clean_footer() 
