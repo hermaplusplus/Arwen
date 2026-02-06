4bfe706a54fe5c6d2c2244b4c879cf3fe26cc2c2
hermaplusplus <49307226+hermaplusplus@users.noreply.github.com>
2026-02-06 01:21:23 +0000

    Add homepage info

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ st.set_page_config(page_title="Arwen DCA", layout="centered")
 
 st.title("Diagnostic Criteria Aggregator")
 
+st.error("This is **not** a diagnostic tool and should not be used as such. Please read the **About** section below for more information.", icon="🚨")
+
+st.markdown("Select a category and disorder, then press the button below to get started:")
+
 cat = st.selectbox("Category", list(data.keys()))
 dis = st.selectbox("Disorder", list(data[cat].keys()))
 
@@ -30,6 +34,10 @@ if st.button("Open Diagnostic Criteria"):
         st.switch_page("pages/simple.py")
 
 st.markdown("## About")
-st.markdown("This tool is designed to help clinicians aggregate diagnostic criteria based on the structure found in teh DSM-5-TR. :red[This is **not** a diagnostic tool and should not be used as such]. Clinicians should always sue their judgement and veri")
+st.markdown("This tool is designed to help clinicians aggregate diagnostic criteria based on the structure found in the DSM-5-TR. :red[This is **not** a diagnostic tool and should not be used as such]. Clinicians should always use their own judgement and verify that criteria and codes are correct.")
+st.markdown("Clincians can select all diagnostic criteria that apply to their patient/client and the tool will output a list of criteria met formatted in a way that can be easily copied into an EHR or note (including systems which parse criteria to :sparkles: :rainbow[automagically*] :sparkles: create a note)")
+st.markdown("This website does **not store any data** that is submitted and **does not allow the input of any Protected Health Information**. When a user refreshes a page or navigates away, all data is irrevocably lost.")
+st.markdown("This project is open source and available on [GitHub](https://github.com/hermaplusplus/Arwen). It is provided under the [MIT License](https://github.com/hermaplusplus/Arwen/blob/main/LICENSE). Contributions, issue reports, feedback, and suggestions are welcome.")
+st.markdown(":grey[:small[* Automagically, meaning using a Large Language Model.]]")
 
 add_clean_footer() 
