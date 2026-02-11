4bf5e9334e8539d3559f73abfd21a922364da44d
hermaplusplus <49307226+hermaplusplus@users.noreply.github.com>
2026-02-11 20:41:02 +0000

    Remove leftover comments on main page

--- main.py
+++ main.py
@@ -20,17 +20,14 @@ cat = st.selectbox("Category", list(data.keys()))
 dis = st.selectbox("Disorder", list(data[cat].keys()))
 
 if st.button(":green[Open Diagnostic Criteria ->]"):
-    # 1. Store the selection so the next page can see it
     st.session_state.current_cat = cat
     st.session_state.current_dis = dis
     st.session_state.current_dis_page = data[cat][dis]["page_path"]
     st.session_state.current_dis_data = data[cat][dis]["data_path"]
     
-    # 2. Forwarding Logic
     logic_type = data[cat][dis].get("logic", "simple")
     
     if logic_type == "complex":
-        # Note: You must provide the path relative to the root
         st.switch_page(st.session_state.current_dis_page)
     else:
         st.switch_page("pages/simple.py")
